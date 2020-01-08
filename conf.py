d41ac981779664438cc29d4f0750c0f762ef23d9
zhangning <zhangning@inhand.com.cn>
2020-01-08 15:22:56 +0800

    update

--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = u'AAA'
+project = u'MobiusPi'
 copyright = u'2019, zhangning'
 author = u'zhangning'
 
@@ -157,19 +157,3 @@ texinfo_documents = [
 ]
 
 
-# -- Options for Epub output -------------------------------------------------
-
-# Bibliographic Dublin Core info.
-epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
