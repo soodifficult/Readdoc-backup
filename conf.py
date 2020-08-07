bb2d1b25fb3bd10faba5f7c04372e4ca6fc8b722
zhangning <zhangning@inhand.com.cn>
2020-08-07 20:36:24 +0800

    test

--- conf.py
+++ conf.py
@@ -39,7 +39,7 @@ release = u'0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_markdown_tables', 'recommonmark',
+    'sphinx-markdown-tables', 'recommonmark',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
