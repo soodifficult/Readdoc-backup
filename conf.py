db60a60819a29ed92cf98bb6076ce7ae746ea0ec
zhangning <zhangning@inhand.com.cn>
2020-08-07 20:43:00 +0800

    test

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_markdown_tables
+import sphinx-markdown-tables
 
 
 # -- Project information -----------------------------------------------------
